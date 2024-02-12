64fbca694b4369ba482c7c964b52a47219fc1869
anthonypweb <2052146@cegepmontpetit.ca>
2024-02-12 16:44:14 -0500

    amélioration du détourage visage

--- server.py
+++ server.py
@@ -11,32 +11,52 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fronta
 app = Flask(__name__)
 
 # Chemin du dossier où les photos seront enregistrées
-UPLOAD_FOLDER = './photos'
+UPLOAD_FOLDER = '../Fun-Karousel-unity/Assets/photos'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Page d'accueil
 @app.route('/')
 def index():
     return render_template('index.html')
-def detect_faces(frame, padding=50):
-    # Convertir l'image en niveaux de gris
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # Détecter les visages dans l'image
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-    # Recadrer chaque visage détecté
-    for (x, y, w, h) in faces:
-        # Ajouter du padding autour des coordonnées du visage
-        x_pad = max(x - padding, 0)
-        y_pad = max(y - padding, 0)
-        w_pad = min(w + 2 * padding, frame.shape[1] - x_pad)
-        h_pad = min(h + 2 * padding, frame.shape[0] - y_pad)
-        # Recadrer le visage avec du padding
-        cropped_face = frame[y_pad:y_pad+h_pad, x_pad:x_pad+w_pad]
-        # Vous pouvez faire quelque chose avec le visage recadré ici
-        # Par exemple, vous pouvez enregistrer chaque visage recadré dans une liste
-        # Ou les traiter d'une autre manière
-    return cropped_face
 
+def remove_background(image_path):
+    # Charger l'image
+    image = image_path
+    
+   # Convertir l'image en niveaux de gris
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # Détection des visages dans l'image
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    
+    # Créer un masque pour le visage
+    mask = np.zeros_like(gray)
+    
+    # Détection du premier visage
+    if len(faces) > 0:
+        (x, y, w, h) = faces[0]
+        
+        # Calculer les nouvelles dimensions de l'image avec le padding
+        new_width = w + 10 # 10 pixels de padding à gauche et à droite
+        new_height = h + 10  # 10 pixels de padding en haut et en bas
+        
+        # Calculer les coordonnées du coin supérieur gauche du rectangle pour le padding
+        x_padding = max(x - 0, 0)
+        y_padding = max(y - 0, 0)
+        
+        # Dessiner un ovale autour du visage et le remplir avec le masque
+        center = (x + w // 2, y + h // 2)
+        axes = (w // 2, h * 2 // 3)  # Ajustement ici, 2/3 de la hauteur pour un axe vertical plus grand
+        cv2.ellipse(mask, center, axes, 0, 0, 360, (255), -1)
+        
+        # Appliquer le masque à l'image
+        result = cv2.bitwise_and(image, image, mask=mask)
+        
+        # Recadrer l'image avec le padding pour obtenir la nouvelle résolution
+        result = result[y_padding:y_padding+new_height, x_padding:x_padding+new_width]
+        
+        return result
 @app.route('/process_image', methods=['POST'])
 def process_image():
     if 'image' in request.json:
@@ -51,11 +71,11 @@ def process_image():
         nparr = np.frombuffer(photo_bytes, np.uint8)
         frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
         # Détecter les visages dans l'image
-        frame_with_faces = detect_faces(frame)
+        frame_with_faces = remove_background(frame)
         # Générer un nom de fichier unique basé sur la date et l'heure actuelles
         now = datetime.now()
         timestamp = now.strftime("%Y%m%d%H%M%S")
-        image_name = f"image_{timestamp}.jpg"
+        image_name = f"image_{timestamp}.png"
         # Enregistrer l'image dans le dossier de téléchargement
         image_path = os.path.join(app.config['UPLOAD_FOLDER'], image_name)
         cv2.imwrite(image_path, frame_with_faces)
